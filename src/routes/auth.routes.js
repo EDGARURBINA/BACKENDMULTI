6534a3a968b202bab5b0208dcb0188ca264b15ab
Draken-101 <leonardofavion@gmail.com>
2024-04-22 20:10:18 +0200

    newaaass

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,6 +7,8 @@ import { accountLimiter } from "../middlewares/rateLimitMiddleware";
 
 router.post("/singin", accountLimiter,authCtrl.singin)
 
+router.post("/singup", accountLimiter,authCtrl.signup)
+
 router.put("/updateAdmin", [accountLimiter],authCtrl.updateAdmin)
 
 
